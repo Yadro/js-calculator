656232616624724fd667b986a71bd188c3fe3194
yadro <yadrone@gmail.com>
2016-12-09 19:08:07 +0300

    small fix

--- src/calc.ts
+++ src/calc.ts
@@ -1,100 +1,108 @@
 
-function parse(str: string) {
-  str = str.split(' ').join('');
-  let items = [];
-  let fragment = str;
-  let res: RegExpMatchArray;
-  while ((res = /(\d+|[+\-*/()])/g.exec(fragment))) {
-    if (res.index != 0) {
-      throw new SyntaxError('Unexpected character: ' + fragment.substr(0, res.index));
-    }
-    if (!isNaN(+res[0])) {
-      items.push(+res[0]);
-    } else {
-      items.push(res[0]);
-    }
-    fragment = fragment.substr(res[0].length);
-  }
-  if (fragment.length) {
-    throw new SyntaxError('Unexpected character: ' + fragment);
-  }
-  return items;
-}
+function calc(input) {
 
-const prior = {
-  '+': 1,
-  '-': 1,
-  '*': 2,
-  '/': 2,
-};
-function toPolNotation(arr: any[]) {
-  let res = [];
-  let operation = [];
-  for (let i = 0; i < arr.length; i++) {
-    let token = arr[i];
-    if (typeof token == "number") {
-      res.push(token)
-    } else {
-      let opLast = operation[operation.length - 1];
-
-      if (token === '(') {
-        operation.push(token);
+  function parse(str: string) {
+    str = str.split(' ').join('');
+    let items = [];
+    let fragment = str;
+    let res: RegExpMatchArray;
+    while ((res = /(\d+|[+\-*/()])/g.exec(fragment))) {
+      if (res.index != 0) {
+        throw new SyntaxError('Unexpected character: ' + fragment.substr(0, res.index));
       }
-      else if (token === ')') {
-        let buf2;
-        while (operation.length && (buf2 = operation.pop()) != '(') {
-          res.push(buf2);
-        }
-        if (buf2 != '(') {
-          throw new Error(`Missing '('`);
-        }
-      }
-      else if (typeof opLast === "string" && prior[opLast] >= prior[token]) {
-        res.push(operation.pop());
-        operation.push(token);
+      if (!isNaN(+res[0])) {
+        items.push(+res[0]);
       } else {
-        operation.push(token)
+        items.push(res[0]);
+      }
+      fragment = fragment.substr(res[0].length);
+    }
+    if (fragment.length) {
+      throw new SyntaxError('Unexpected character: ' + fragment);
+    }
+    return items;
+  }
+
+  const prior = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+  };
+
+  function toPolNotation(arr: any[]) {
+    let res = [];
+    let operation = [];
+    for (let i = 0; i < arr.length; i++) {
+      let token = arr[i];
+      if (typeof token == "number") {
+        res.push(token)
+      } else {
+        let opLast = operation[operation.length - 1];
+
+        if (token === '(') {
+          operation.push(token);
+        }
+        else if (token === ')') {
+          let buf2;
+          while (operation.length && (buf2 = operation.pop()) != '(') {
+            res.push(buf2);
+          }
+          if (buf2 != '(') {
+            throw new Error(`Missing '('`);
+          }
+        }
+        else if (typeof opLast === "string" && prior[opLast] >= prior[token]) {
+          res.push(operation.pop());
+          operation.push(token);
+        } else {
+          operation.push(token)
+        }
       }
     }
+    while (operation.length) {
+      res.push(operation.pop())
+    }
+    return res;
   }
-  while (operation.length) {
-    res.push(operation.pop())
-  }
-  return res;
-}
 
-const operations = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b
-};
-function cmCalc(arr: number[], op) {
-  let b = arr.pop();
-  let a = arr.pop();
-  if (operations[op]) {
-    arr.push(operations[op](a, b));
-  } else {
-    throw new Error('Wrong input');
-  }
-  return arr;
-}
-function calc(polsk: any[]) {
-  let res = [];
-  while (polsk.length) {
-    let el = polsk.shift();
-    if (typeof el === "number") {
-      res.push(el);
-    } else if (res.length > 1) {
-      res = cmCalc(res, el)
+  const operations = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b
+  };
+
+  function cmCalc(arr: number[], op) {
+    let b = arr.pop();
+    let a = arr.pop();
+    if (operations[op]) {
+      arr.push(operations[op](a, b));
     } else {
       throw new Error('Wrong input');
     }
+    return arr;
   }
-  if (res.length != 1) {
-    throw new Error('Wrong input');
+
+  function calc(polsk: any[]) {
+    let res = [];
+    while (polsk.length) {
+      let el = polsk.shift();
+      if (typeof el === "number") {
+        res.push(el);
+      } else if (res.length > 1) {
+        res = cmCalc(res, el)
+      } else {
+        throw new Error('Wrong input');
+      }
+    }
+    if (res.length != 1) {
+      throw new Error('Wrong input');
+    }
+    return res.pop();
   }
-  return res.pop();
+
+  return calc(toPolNotation(parse(input)));
 }
 
 (() => {
@@ -104,7 +112,7 @@ function calc(polsk: any[]) {
   input.addEventListener('input', () => {
     try {
       if (input.value) {
-        result.innerText = calc(toPolNotation(parse(input.value)));
+        result.innerText = calc(input.value);
       } else {
         result.innerText = '';
       }
